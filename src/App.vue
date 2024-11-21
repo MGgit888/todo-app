1c0529008feec74084b6e23cef121063fc816c4a
MGgit888 <77639566+MGgit888@users.noreply.github.com>
2024-11-21 13:02:33 -0500

    ToDo list create

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    <div>
+      <h1>To-Do List</h1>
+      <input type="text" v-model="newTodo" @keyup.enter="addTodo">
+      <ul>
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.text }}
+          <button @click="removeTodo(todo.id)">Remove</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newTodo: '',
+        todos: [
+          { id: 1, text: 'Learn Vue' },
+          { id: 2, text: 'Build a To-Do App' }
+        ]
+      }
+    },
+    methods: {
+      addTodo() {
+        if (this.newTodo.trim()) {
+          this.todos.push({ id: Date.now(), text: this.newTodo });
+          this.newTodo = '';
+        }
+      },
+      removeTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id);
+      }
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
